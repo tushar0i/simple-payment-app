45d5d8ffcfc9184879f04c1cdc602e9530476d8f
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-11-15 16:36:11 +0530

    Adding userSchema

---
+++ backEnd/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require(mongoose);
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName : String,
+    lastName : String,
+    email : String,
+    password : String,
+});
+
+const userModel = mongoose.model("user",userSchema)
+
+module.exports = {
+    userModel,
+}

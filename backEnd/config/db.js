7d1714a6119888b165db9121c980e44068cc6627
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-11-16 22:49:57 +0530

    updating user schema

--- backEnd/config/db.js
+++ backEnd/config/db.js
@@ -1,11 +1,31 @@
-const mongoose = require(mongoose);
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    firstName : String,
-    lastName : String,
-    email : String,
-    password : String,
+    firstName : {
+        type: String,
+        required: true,
+        trim: true,
+        minLength:1 
+    },
+    lastName : {
+        type: String,
+        required: true,
+        trim: true,
+        minLength:1 
+    },
+    email : {
+        type: String,
+        required: true,
+        trim: true,
+        unique:true,
+        lowercase:true
+    },
+    password : {
+        type:String,
+        required:true,
+
+    },
 });
 
 const userModel = mongoose.model("user",userSchema)

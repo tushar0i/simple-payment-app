2c2a3610a5b76eda8fbf4db51108682133cc9099
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-11-27 22:27:37 +0530

    Adding account Schema

--- backEnd/config/db.js
+++ backEnd/config/db.js
@@ -28,8 +28,25 @@ const userSchema = new Schema({
     },
 });
 
-const userModel = mongoose.model("user",userSchema)
+const accountSchema = new Schema({
+    userId : {
+        type : Schema.Types.ObjectId,
+        ref: 'User',
+        required:true
+    },
+    balance : {
+        type: Number,
+        // We Will store integer insted of float because if accuracy 
+        // 8.95 -> will be stored as 895
+        // 90 -> 9000
+        required:true
+    }
+});
+
+const User = mongoose.model("User",userSchema)
+const Account = mongoose.model("Account",accountSchema)
 
 module.exports = {
-    userModel,
+    User,
+    Account
 }

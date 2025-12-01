32fa61d630556ba632b52570eb724426d3d82611
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-12-01 23:41:10 +0530

    Adding sign in page(temp)

--- frontEnd/pages/Signin.jsx
+++ frontEnd/pages/Signin.jsx
@@ -1,6 +1,14 @@
+import { Heading } from "../components/Heading";
+import { InputBox } from "../components/InputBox";
 
 export function Sighin(){
     return(<>
-
+    <div className="flex justify-center items-center h-screen">
+        <div className="h-3/4 w-sm  bg-gray-100 rounded-2xl border-3 shadow-sm flex flex-col items-center gap-4">
+        <Heading  className="" content={"Sigh In"}></Heading>
+        <InputBox className="" place={"example@email.com"} label={"Email"} type={"text"}></InputBox>
+        <InputBox className="" place={"Nothing@123"} label={"Password"} type={"password"}></InputBox>
+        </div>
+    </div>
     </>)
 }

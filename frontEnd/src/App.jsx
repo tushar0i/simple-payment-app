f7702485b273e52f9ebfd5e540ee6dfc69813bcf
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-12-02 18:55:15 +0530

    Adding routes

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter , Routes, Route} from 'react-router-dom'
 import './App.css'
-import { Heading } from '../components/Heading'
+import { Signup } from "../pages/Signup"
+import { Signin } from "../pages/Signin"
+import { Dashboard } from "../pages/Dashboard"
+import { SendMoney } from "../pages/SendMoney"
 function App() {
 
   return (
     <>
-    <Heading content={"Hi this is tushar"}></Heading>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/signup" element={<Signup/>}></Route>
+            <Route path="/signin" element={<Signin/>}></Route>
+            <Route path="/dashboard" element={<Dashboard/>}></Route>
+            <Route path="/send" element={<SendMoney/>}></Route>
+        </Routes>
+    </BrowserRouter>
     </>
   )
 }

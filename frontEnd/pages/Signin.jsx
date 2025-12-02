c7407d2446fd52f0ff8558145eb97b26f6eada4f
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-12-02 18:56:30 +0530

    Adding SIGN IN page

--- frontEnd/pages/Signin.jsx
+++ frontEnd/pages/Signin.jsx
@@ -1,14 +1,20 @@
 import { Heading } from "../components/Heading";
 import { InputBox } from "../components/InputBox";
+import { SubHeading } from "../components/SubHeading";
+import { Button } from "../components/Button";
+import { BottomText } from "../components/BottomText";
 
-export function Sighin(){
+export function Signin(){
     return(<>
     <div className="flex justify-center items-center h-screen">
-        <div className="h-3/4 w-sm  bg-gray-100 rounded-2xl border-3 shadow-sm flex flex-col items-center gap-4">
-        <Heading  className="" content={"Sigh In"}></Heading>
-        <InputBox className="" place={"example@email.com"} label={"Email"} type={"text"}></InputBox>
-        <InputBox className="" place={"Nothing@123"} label={"Password"} type={"password"}></InputBox>
+            <div className="w-sm  bg-gray-100 rounded-2xl border-3 shadow-sm flex flex-col items-center gap-4">
+            <Heading  className="" content={"Sigh In"}></Heading>
+            <SubHeading content={"Enter your details to sign in to your account"}></SubHeading>
+            <InputBox className="" place={"someone@email.com"} label={"Email"} type={"text"}></InputBox>
+            <InputBox className="" place={"Nothing@123"} label={"Password"} type={"password"}></InputBox>
+            <Button label={"Sign In"}></Button>
+            <BottomText text={"Don’t have an account yet? "} link={"/signup"} page={"Signup"}></BottomText>
+            </div>
         </div>
-    </div>
     </>)
 }

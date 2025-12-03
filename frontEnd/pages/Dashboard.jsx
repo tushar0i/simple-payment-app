9c19da297a686b20fb0e4db48a8d5ea8f5667925
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-12-04 02:56:12 +0530

    Adding Dashboard page (temp)

--- frontEnd/pages/Dashboard.jsx
+++ frontEnd/pages/Dashboard.jsx
@@ -1,4 +1,13 @@
+import { SearchBar } from "../components/SearchBar"
+import { ShowBalance } from "../components/ShowBalance"
+import { TopBar } from "../components/TopBar"
 export function Dashboard(){
     return(<>
+
+    <TopBar firstName={"Someone"}></TopBar>
+    <div className="flex gap-4 mt-20 ">
+    <ShowBalance balance={200} ></ShowBalance>
+    <SearchBar  type={"text"} place={"Search User here ...." } ></SearchBar>
+    </div>
     </>)
 }

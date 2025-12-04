a9d425ece5dcabc3c0653ae656377ee7be82dae2
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-12-05 00:22:56 +0530

    Fetching data from backend for Dashboard

--- frontEnd/pages/Dashboard.jsx
+++ frontEnd/pages/Dashboard.jsx
@@ -1,13 +1,53 @@
+import { useNavigate } from "react-router-dom"
 import { SearchBar } from "../components/SearchBar"
 import { ShowBalance } from "../components/ShowBalance"
 import { TopBar } from "../components/TopBar"
-export function Dashboard(){
-    return(<>
+import { UsersList } from "../components/UsersList"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { GihubRepo } from "../components/GithubRepo"
 
-    <TopBar firstName={"Someone"}></TopBar>
-    <div className="flex gap-4 mt-20 ">
-    <ShowBalance balance={200} ></ShowBalance>
-    <SearchBar  type={"text"} place={"Search User here ...." } ></SearchBar>
-    </div>
+export function Dashboard() {
+    const [filter, setFilter] = useState([])
+    const [users, setUser] = useState([])
+    const [balance, setBalance] = useState("")
+    const token = localStorage.getItem("token");
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/v1/user/bulk?filter=" + filter, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(response => {
+                setUser(response.data.user)
+            })
+    }, [filter]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/v1/account/balance", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+            .then(response => {
+                setBalance(response.data.balance)
+            })
+    }, [])
+    const navigate = useNavigate()
+    return (<>
+        <TopBar firstName={"Someone"} onClick={() => {
+            localStorage.removeItem("token")
+            navigate("/signin")
+        }}></TopBar>
+        <div className="flex gap-4 mt-20 ">
+            <ShowBalance balance={balance} ></ShowBalance>
+            <SearchBar type={"text"} place={"Search User here ...."} onChange={(e) => {
+                setFilter(e.target.value)
+            }} ></SearchBar>
+        </div>
+        <div className="m-4">
+            {users.map(user => <UsersList user={user} ></UsersList>)}
+        </div>
+        <GihubRepo></GihubRepo>
     </>)
 }

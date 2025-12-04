9a0d5395c5482e5d4d8f1585338f04292fb98fd6
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-12-04 23:50:28 +0530

    Small design changes for responsiveness

--- frontEnd/components/SearchBar.jsx
+++ frontEnd/components/SearchBar.jsx
@@ -1,12 +1,12 @@
-export function SearchBar({ place,type }) {
+export function SearchBar({ place,type,onChange }) {
     return (<>
-        <div className=" bg-gray-200 rounded-lg  text-black  grow mr-4  shadow-gray-400 shadow-md/30">
+        <div className=" bg-gray-200 rounded-lg  text-black  grow mr-4 ">
             <div className="flex items-center gap-4 px-5 py-2.5">
                 <div className="text-black text-xl font-bold hidden sm:block">
                     USERS
                 </div>
                 <div className="flex grow">
-                    <input type={type} placeholder={place} className="text-black rounded-md  w-full px-3 py-1 placeholder-gray-500  outline-2 outline-black/40 shadow-sm  "/>
+                    <input type={type} placeholder={place} className="text-black rounded-md  w-full px-3 py-1 placeholder-gray-500  outline-2 outline-black/40 shadow-sm " onChange={onChange}/>
                 </div>
             </div>
         </div>
--- frontEnd/components/ShowBalance.jsx
+++ frontEnd/components/ShowBalance.jsx
@@ -1,10 +1,10 @@
 export function ShowBalance({balance}){
     return (<>
     <div className="text-black text-xl font-medium flex justify">
-        <div className=" bg-gray-200 px-5 py-3 rounded-lg  ml-4 shadow-gray-400 shadow-md/30">Your balance : ${(balance / 100).toLocaleString("en-US", {
+        <div className=" bg-gray-200 px-5 py-3 rounded-lg  ml-4 ">Your balance : <span className="text-green-600">${(balance / 100).toLocaleString("en-US", {
         minimumFractionDigits: 2,
         maximumFractionDigits: 2
-    })}</div>
+    })}</span> </div>
         
     </div>
     </>)

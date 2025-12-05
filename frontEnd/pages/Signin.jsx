ddc301ff36db07498618eef3365e3a6803708e61
tushar0i <171576246+tushar0i@users.noreply.github.com>
2025-12-05 23:50:59 +0530

    Adding new password box & err alerts

--- frontEnd/pages/Signin.jsx
+++ frontEnd/pages/Signin.jsx
@@ -7,34 +7,55 @@ import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { GihubRepo } from "../components/GithubRepo";
+import { PasswordInputBox } from "../components/PasswordInputBox";
 
-export function Signin(){
+export function Signin() {
+
     const navigate = useNavigate();
-    const [email,setEmail] = useState("")
-    const [password,setPassword] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
-    return(<>
-    <div className="flex justify-center items-center h-screen">
+    return (<>
+
+        <div className="flex justify-center items-center h-screen">
+
             <div className="w-sm  bg-gray-100 rounded-2xl border-3 shadow-sm flex flex-col items-center gap-4">
-            <Heading  className="" content={"Sigh In"}></Heading>
-            <SubHeading content={"Enter your details to sign in to your account"}></SubHeading>
-            <InputBox className="" place={"someone@email.com"} label={"Email"} type={"text"} onChange={(e)=>{
-                setEmail(e.target.value)
-            }}></InputBox>
-            <InputBox className="" place={"Nothing@123"} label={"Password"} type={"password"} onChange={(e)=>{
-                setPassword(e.target.value)
-            }}></InputBox>
-            <Button label={"Sign In"} onClick={async ()=>{
-                const response = await axios.post("http://localhost:3000/api/v1/user/signin",{
-                    email,
-                    password
-                });
-                localStorage.setItem("token",response.data.token)
-                navigate("/dashboard");
-            }}></Button>
-            <BottomText text={"Don't have an account yet? "} link={"/signup"} page={"Signup"}></BottomText>
+                {/* just realized I was using Sigh insted of sign */}
+                <Heading className="" content={"Sign In"}></Heading>
+
+                <SubHeading content={"Enter your details to sign in to your account"}></SubHeading>
+
+                <InputBox className="" place={"someone@email.com"} label={"Email"} type={"text"} onChange={(e) => {
+                    setEmail(e.target.value)
+                }}></InputBox>
+
+                <PasswordInputBox place={"Nothing@123"} label={"Password"} onChange={(e) => {
+                    setPassword(e.target.value);
+                }}></PasswordInputBox>
+
+                <Button label={"Sign In"} onClick={() => {
+                    axios.post("http://localhost:3000/api/v1/user/signin", {
+                        email,
+                        password
+                    })
+                        .then(response => {
+                            localStorage.setItem("token", response.data.token)
+                            navigate("/dashboard");
+                        })
+                        .catch(err => {
+                            console.log(err)
+                            alert(err.response.data.message)
+                        })
+
+                }}></Button>
+
+                <BottomText text={"Don't have an account yet? "} link={"/signup"} page={"Signup"}></BottomText>
+
             </div>
+
         </div>
+
         <GihubRepo></GihubRepo>
+        
     </>)
 }
--- frontEnd/pages/Signup.jsx
+++ frontEnd/pages/Signup.jsx
@@ -7,43 +7,66 @@ import { useState } from "react";
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import { GihubRepo } from "../components/GithubRepo";
+import { PasswordInputBox } from "../components/PasswordInputBox";
+
 
 export function Signup(){
+
     const navigate = useNavigate()
     const[firstName , setFirstName] = useState("");
     const[lastName , setLastName] = useState("");
     const[email , setEmail] = useState("");
     const[password , setPassword] = useState("");
+
     return(<>
+
     <div className="flex justify-center items-center h-screen">
+
         <div className="w-sm  bg-gray-100 rounded-2xl border-3 shadow-sm flex flex-col items-center gap-4">
-        <Heading  className="" content={"Sigh Up"}></Heading>
+
+        <Heading  className="" content={"Sign Up"}></Heading> 
+
         <SubHeading content={"Enter your details to create an account"}></SubHeading>
+
         <InputBox className="" place={"Some"} label={"First Name"} type={"text"} onChange={(e)=>{
             setFirstName(e.target.value);
         }}></InputBox>
+        
         <InputBox className="" place={"One"} label={"Last Name"} type={"text"} onChange={(e)=>{
             setLastName(e.target.value);
         }}></InputBox>
+
         <InputBox className="" place={"someone@email.com"} label={"Email"} type={"text"} onChange={(e)=>{
             setEmail(e.target.value);
         }}></InputBox>
-        <InputBox className="" place={"Nothing@123"} label={"Password"} type={"password"} onChange={(e)=>{
+
+        <PasswordInputBox place={"Nothing@123"} label={"Password"} onChange={(e)=>{
             setPassword(e.target.value);
-        }}></InputBox>
-        <Button label={"Sign Up"} onClick={ async ()=>{
-            const response = await axios.post("http://localhost:3000/api/v1/user/signup",{
+        }}></PasswordInputBox>
+
+        <Button label={"Sign Up"} onClick={ ()=>{
+            axios.post("http://localhost:3000/api/v1/user/signup",{
                 email:email,
                 password:password,
                 firstName:firstName,
                 lastName:lastName
-            });
-            localStorage.setItem("token",response.data.token)
-            navigate("/dashboard");
+            })
+            .then(response=>{
+                localStorage.setItem("token",response.data.token)
+                navigate("/dashboard");
+            })
+            .catch((err)=>{
+                console.log(err)
+                alert(err.response.data.message)
+            })
         }}></Button>
+
         <BottomText text={"Already have an account? "} link={"/signin"} page={"Signin"}></BottomText>
+
         </div>
     </div>
+
     <GihubRepo></GihubRepo>
+    
     </>)
 }
